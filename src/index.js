1d6ce13ca6c1b6af88911dfd207d3e6eace33c94
apotatostory <apotatostory@gmail.com>
2018-04-15 01:52:13 +0800

    my resume v1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './css/index.css';
+import './css/App.css';
+import './css/all.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
